3b570e08394588fec1447068f6dcb4dba701525b
faqiih256 <145763727+faqiih256@users.noreply.github.com>
2024-11-11 21:53:47 +0700

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,34 +1,26 @@
-importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js");
+importScripts('https://www.gstatic.com/firebasejs/9.16.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.16.0/firebase-messaging-compat.js');
 
-// Inisialisasi Firebase hanya jika belum dilakukan
-if (!firebase.apps.length) {
-  firebase.initializeApp({
-    apiKey: "AIzaSyBzmhQpQtA94ZFGINfGoBLFYfAv279miX4",
-    authDomain: "ahmadnurfakih-4d633.firebaseapp.com",
-    projectId: "ahmadnurfakih-4d633",
-    storageBucket: "ahmadnurfakih-4d633.firebasestorage.app",
-    messagingSenderId: "107790419368",
-    appId: "1:107790419368:web:3807d72f8c413629d0499d",
-    measurementId: "G-DXKYW1539T"
-  });
-}
+firebase.initializeApp({
+  apiKey: "AIzaSyBzmhQpQtA94ZFGINfGoBLFYfAv279miX4",
+  authDomain: "ahmadnurfakih-4d633.firebaseapp.com",
+  projectId: "ahmadnurfakih-4d633",
+  storageBucket: "ahmadnurfakih-4d633.firebasestorage.app",
+  messagingSenderId: "107790419368",
+  appId: "1:107790419368:web:3807d72f8c413629d0499d",
+  measurementId: "G-DXKYW1539T"
+});
 
 const messaging = firebase.messaging();
 
-
-messaging.onBackgroundMessage(function(payload) {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-
-
-  const notification = payload.data.notification ? JSON.parse(payload.data.notification) : { title: "New Message", body: "You have a new message." };
-
-  const notificationTitle = notification.title;
+// Handle background messages
+messaging.onBackgroundMessage((payload) => {
+  console.log('Received background message: ', payload);
+  const notificationTitle = payload.notification.title;
   const notificationOptions = {
-    body: notification.body,
-    icon: 'images/Lonceng.png' 
+    body: payload.notification.body,
+    icon: 'images/Lonceng.png'
   };
 
-
-  return self.registration.showNotification(notificationTitle, notificationOptions);
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
