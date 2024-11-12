8850171598fdd6f74ef35ef37e1337e940ec153f
faqiih256 <145763727+faqiih256@users.noreply.github.com>
2024-11-12 10:58:01 +0700

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,28 +1,31 @@
 importScripts('https://www.gstatic.com/firebasejs/9.16.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.16.0/firebase-messaging-compat.js');
 
-firebase.initializeApp({
-  apiKey: "AIzaSyBzmhQpQtA94ZFGINfGoBLFYfAv279miX4",
-  authDomain: "ahmadnurfakih-4d633.firebaseapp.com",
-  projectId: "ahmadnurfakih-4d633",
-  storageBucket: "ahmadnurfakih-4d633.firebasestorage.app",
-  messagingSenderId: "107790419368",
-  appId: "1:107790419368:web:3807d72f8c413629d0499d",
-  measurementId: "G-DXKYW1539T"
-});
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: "AIzaSyBzmhQpQtA94ZFGINfGoBLFYfAv279miX4",
+    authDomain: "ahmadnurfakih-4d633.firebaseapp.com",
+    projectId: "ahmadnurfakih-4d633",
+    storageBucket: "ahmadnurfakih-4d633.firebasestorage.app",
+    messagingSenderId: "107790419368",
+    appId: "1:107790419368:web:3807d72f8c413629d0499d",
+    measurementId: "G-DXKYW1539T"
+  });
+}
 
 const messaging = firebase.messaging();
-messaging.setBackgroundMessageHandler(function(payload) {
-  console.log(
-    '[firebase-messaging-sw.js] Received background message ',
-    payload
-  );
-// Handle background messages
-  const notificationTitle = payload.notification.title;
+
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  const notification = payload.data.notification ? JSON.parse(payload.data.notification) : { title: "New Message", body: "You have a new message." };
+
+  const notificationTitle = notification.title;
   const notificationOptions = {
-    body: payload.notification.body,
-    icon: 'images/Lonceng.png'
+    body: notification.body,
+    icon: 'images/Lonceng.png' 
   };
 
-  self.registration.showNotification(notificationTitle, notificationOptions);
+
+  return self.registration.showNotification(notificationTitle, notificationOptions);
 });
