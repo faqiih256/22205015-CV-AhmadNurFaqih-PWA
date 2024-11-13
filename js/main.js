639b8989c4830695c63828b8268474a5477ac51a
faqiih256 <145763727+faqiih256@users.noreply.github.com>
2024-11-13 14:54:57 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js')
-        .then((registration) => {
-            console.log('Service Worker registered with scope:', registration.scope);
-        })
-        .catch((error) => {
-            console.log('Service Worker registration failed:', error);
-        });
+window.onload = () => {
+    'use strict';
+  
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+               .register('sw.js');
+    }
 }
-
 
 const burger = document.querySelector('.burger');
 const navLinks = document.querySelector('.nav-links');
