e23aab45180c013e43ca4b6a3d5b49a788bc3a3f
faqiih256 <145763727+faqiih256@users.noreply.github.com>
2024-11-13 20:57:18 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,61 +1,53 @@
-const cacheName = 'cv-pwa';
-const filesToCache = [
+const CACHE_NAME = 'cv-pwa';
+const urlsToCache = [
     '/',
     'index.html',
-    'manifest.json',
-    'notifikasi.js',
     'styles.css',
-    'sw.js',
-    'images/Desktop.png',
+    'manifest.json',
+    'firebase-messaging-sw.js',
+    'main.js',
     'images/Fakih.jpeg',
-    'images/Logo_CV.png',
     'images/Logo_CV_128.png',
     'images/Logo_CV_144.png',
     'images/Logo_CV_152.png',
     'images/Logo_CV_192.png',
     'images/Logo_CV_256.png',
     'images/Logo_CV_512.png',
+    'images/Logo_CV.png',
     'images/Lonceng.png',
-    'images/Mobile.png',
     'js/indexdb.js',
     'js/main.js'
 ];
 
-
-
-
-// Install event to cache files
-self.addEventListener('install', function(e) {
-    e.waitUntil(
-      caches.open(cacheName).then(function(cache) {
-        return cache.addAll(filesToCache);
-      })
+// Install Service Worker
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
     );
-    self.skipWaiting();
 });
 
-// Activate event to clear old caches
-self.addEventListener('activate', function(e) {
-    e.waitUntil(
-        caches.keys().then(function(cacheNames) {
+// Fetch cached assets
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
+    );
+});
+
+// Activate new cache
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
             return Promise.all(
-                cacheNames.map(function(name) {
-                    if (name !== cacheName) {
-                        return caches.delete(name);
-                    }
-                })
+                cacheNames.filter((cacheName) => {
+                    return cacheName !== CACHE_NAME;
+                }).map((cacheName) => caches.delete(cacheName))
             );
-        })
-    );
-    self.clients.claim();
-});
-
-// Fetch event to serve cached files when offline
-self.addEventListener('fetch', function(e) {
-    e.respondWith(
-        caches.match(e.request).then(function(response) {
-            // Serve from cache if available, otherwise fetch from network
-            return response || fetch(e.request);
         })
     );
 });
