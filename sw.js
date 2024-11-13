607c8f70ad91fc3ef5953232adfd010374ccc219
faqiih256 <145763727+faqiih256@users.noreply.github.com>
2024-11-13 20:29:17 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,40 +1,39 @@
-const cacheName = 'cv-pwa';
-const filesToCache = [
-    '/',
-    'index.html',
-    'manifest.json',
-    'notifikasi.js',
-    'styles.css',
-    'sw.js',
-    'images/Desktop.png',
-    'images/Fakih.jpeg',
-    'images/Logo_CV.png',
-    'images/Logo_CV_128.png',
-    'images/Logo_CV_144.png',
-    'images/Logo_CV_152.png',
-    'images/Logo_CV_192.png',
-    'images/Logo_CV_256.png',
-    'images/Logo_CV_512.png',
-    'images/Lonceng.png',
-    'images/Mobile.png',
-    'js/indexdb.js',
-    'js/main.js'
+var cacheName = 'app-shell';
+var filesToCache = [
+  '/',
+  'index.html',
+  'style.css',
+  'manifest.json',
+  'main.js',
+  'firebase-messaging-sw.js',
+  'images/Fakih.jpeg',
+  'images/Logo_CV_128.png',
+  'images/Logo_CV_144.png',
+  'images/Logo_CV_152.png',
+  'images/Logo_CV_192.png',
+  'images/Logo_CV_256.png',
+  'images/Logo_CV_512.png',
+  'images/Logo_CV.png',
+  'images/Lonceng.png',
+  'js/main.js',
+  'js/indexdb.js'
 ];
 
+/* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
-    e.waitUntil(
-      caches.open(cacheName).then(function(cache) {
-        return cache.addAll(filesToCache);
-      })
-    );
-    self.skipWaiting();
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+  self.skipWaiting();
 });
-  
 
+/* Serve cached content when offline */
 self.addEventListener('fetch', function(e) {
-    e.respondWith(
-      caches.match(e.request).then(function(response) {
-        return response || fetch(e.request);
-      })
-    );
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
 });
