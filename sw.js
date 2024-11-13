e3ab9136afaa0d85404c4be25575a901a053046c
faqiih256 <145763727+faqiih256@users.noreply.github.com>
2024-11-13 20:07:48 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -23,12 +23,12 @@ const filesToCache = [
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
-  self.skipWaiting();
+    e.waitUntil(
+      caches.open(cacheName).then(function(cache) {
+        return cache.addAll(filesToCache);
+      })
+    );
+    self.skipWaiting();
 });
 
 /* Serve cached content when offline */
