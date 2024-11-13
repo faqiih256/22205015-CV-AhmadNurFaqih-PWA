1cb12ee0d7d3bde2f1c9bdf88f1ebff2cbcb5170
faqiih256 <145763727+faqiih256@users.noreply.github.com>
2024-11-13 21:18:57 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
-window.onload = () => {
-    'use strict';
-  
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker
-               .register('sw.js');
-    }
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js')
+        .then((registration) => {
+            console.log('Service Worker registered with scope:', registration.scope);
+        })
+        .catch((error) => {
+            console.log('Service Worker registration failed:', error);
+        });
 }
+
+
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
